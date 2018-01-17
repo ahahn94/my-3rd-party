a7a26a4c5c9c79e043038cc5b94dddd5dfa82146
ahahn94 <ahahn94@outlook.com>
2018-01-17 14:48:21 +0100

    Update GoLand 2017.3.1
    Update GoLand to version 2017.3.1 from January 8, 2018.
    
    Tests:
    Build, Install, Execution
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/goland/actions.py
+++ programming/goland/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 Version =  get.srcVERSION()
 
 def install():
-    shutil.rmtree("GoLand-%s/jre64" % Version)
-    pisitools.insinto("/opt/goland", "GoLand-%s/*" % Version)
+    shutil.rmtree("GoLand-2017.3/jre64")
+    pisitools.insinto("/opt/goland", "GoLand-*/*")
     pisitools.dosym("/opt/goland/bin/goland.sh", "/usr/bin/goland")
