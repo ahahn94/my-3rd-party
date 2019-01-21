b253c5967de06aca2f26c8cc2afd26e3428bb149
ahahn94 <ahahn94@outlook.com>
2019-01-21 14:27:08 +0100

    Update Synology Cloud Station Backup 4.2.8-4421
    Update Synology Cloud Station Backup to version 4.2.8-4421 from 2018-09-03.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- network/download/synology-cloud-station-backup/actions.py
+++ network/download/synology-cloud-station-backup/actions.py
@@ -9,7 +9,7 @@ NoStrip = ["/"]
 IgnoreAutodep = True
 
 def setup():
-    shelltools.system("ar xf synology-cloud-station-backup-4396.x86_64.deb")
+    shelltools.system("ar xf synology-cloud-station-backup-4421.x86_64.deb")
     shelltools.system("tar xf data.tar.gz")
 
 def install():
